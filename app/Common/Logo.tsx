392c0114fd0d4da84858f34b8d6711d5f41ef910
Mubarek Shikur <manmuba90@gmail.com>
2023-09-14 13:13:37 +0300

    Logo

---
+++ app/Common/Logo.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import Image from "next/image";
+
+interface LogoProps {
+    isDark: boolean;
+}
+
+const Logo: React.FC<LogoProps> = ({
+    isDark
+}) => {
+    return (<>
+        {isDark ?
+            <Image
+                className=""
+                height="110"
+                width="110"
+                alt="Avatar"
+                src="/images/logo-light.png"
+            /> : 
+            <Image
+                className=""
+                height="110"
+                width="110"
+                alt="Avatar"
+                src="/images/logo-dark.png"
+            />
+            }
+    </>
+    )
+}
+
+export default Logo
---
+++ app/Layout/Header.tsx
@@ -0,0 +1,13 @@
+import Logo from "../Common/Logo"
+
+
+const Header = () => {
+    const isDark = false
+  return (
+    <div>
+        <Logo isDark={isDark}/>
+    </div>
+  )
+}
+
+export default Header
