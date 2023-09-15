954ab8fe6f946d28891fada934331e51a47ab187
Mubarek Shikur <manmuba90@gmail.com>
2023-09-15 16:02:26 +0300

    Quantity on Product Details

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import HomePage from './Pages/HomePage'
+import ProductDetail from './Pages/ProductDetail'
 import Store from './Pages/Store'
 
 export default function Home() {
   return (
     <main>
-      <Store />
+      <ProductDetail />
     </main>
   )
 }
