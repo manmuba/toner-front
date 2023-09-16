cac35c911b908fcb92b751305f5b854c34f25f1b
Mubarek Shikur <manmuba90@gmail.com>
2023-09-15 22:26:31 -0700

    Success Page on progress

---
+++ app/Pages/CreateSuccess.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import Image from "next/image";
+import Footer from "../Layout/Footer";
+import Header from "../Layout/Header";
+import "bootstrap/dist/css/bootstrap.css";
+
+const CreateSuccess = () => {
+  return (
+    <div className=" bg-neutral-200">
+      <Header />
+      <div className="flex w-[90vw] mx-[5vw] md:mx-[30vw] md:w-[40vw] rounded-lg mb-6 flex-col my-5 bg-white">
+        <div className="flex bg-blue-600 w-full p-12 gap-12 rounded-t-lg">
+          <Image
+            width="200"
+            height="200"
+            alt="Image"
+            src="/images/auth/img-1.png"
+            className="w-[100px] md:w-[150px] lg:w-[200px]"
+          />
+          <h1 className="text-white text-xl md:text-3xl">Forgot Password?</h1>
+        </div>
+        <div className="flex-col p-5 items-center justify-center">
+          <h5 className="text-neutral-400 text-[14px] font-semibold">
+            Reset password with Toner.
+          </h5>
+          <div className="p-2 items-center flex justify-center w-[95%] mx-4 alert alert-borderless alert-warning text-center mt-4">
+            <span className="text-yellow-600 text-[14px]">
+              Enter your email and instructions will be sent to you!
+            </span>
+          </div>
+          <h4 className="mx-4 mb-3 text-[14px] font-semibold mt-6">Email</h4>
+          <input
+            type="email"
+            className="mx-4 p-2 px-5 w-[95%] outline-none focus:border-blue-500 border rounded-md"
+            placeholder="Enter your email or username "
+          />
+
+          <button className="mx-4 my-4 w-[95%] bg-green-600 hover:bg-blue-500 transition delay-75 text-white p-2 rounded-md text-[14px]">
+            Back to DashBoard
+          </button>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default CreateSuccess;
