5f7fff4602c79b7707a64a329e070829ee0e9548
Mubarek Shikur <manmuba90@gmail.com>
2023-09-15 21:51:05 -0700

    SignUp on progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
-import HomePage from './Pages/HomePage'
-import ProductDetail from './Pages/ProductDetail'
-import SignIn from './Pages/SignIn'
-import Store from './Pages/Store'
+import HomePage from "./Pages/HomePage";
+import ProductDetail from "./Pages/ProductDetail";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import Store from "./Pages/Store";
 
 export default function Home() {
   return (
     <main>
       {/* <HomePage /> */}
-      <SignIn />
+      {/* <SignIn /> */}
+      <SignUp />
       {/* <Store />
       <ProductDetail /> */}
     </main>
-  )
+  );
 }
